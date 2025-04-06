6df5f1f0f2c315bba17356f1363546f028755cae
awsmind <justincook6@icloud.com>
2025-04-06 10:21:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,23 +19,37 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
-  name   = var.name
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = var.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 module "eks" {
-  source       = "./modules/eks"
-  name         = var.name
-  vpc_id       = module.vpc.vpc_id
-  subnet_ids   = module.vpc.private_subnet_ids
-  cluster_name = "${var.name}-cluster"
-}
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
 
-module "ec2_worker" {
-  source       = "./modules/ec2_worker"
-  name         = var.name
-  subnet_id    = module.vpc.public_subnet_ids[0]
-  cluster_name = module.eks.cluster_name
+  cluster_name    = "${var.name}-cluster"
+  cluster_version = "1.28"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+    }
+  }
 }
 
 resource "kubernetes_deployment" "web" {
@@ -62,7 +76,7 @@ resource "kubernetes_deployment" "web" {
       spec {
         container {
           name  = "web"
-          image = "nginx" # Replace with your container image
+          image = "nginx"
           ports {
             container_port = 80
           }
@@ -90,19 +104,3 @@ resource "kubernetes_service" "web" {
     }
   }
 }
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
-
-  name = var.name
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
