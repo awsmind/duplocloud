87ee9fc78897a6463ef1fe55e3af9b7d0f52afc2
awsmind <justincook6@icloud.com>
2025-04-06 11:02:13 -0700

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,1 +1,16 @@
+# modules/eks/variables.tf
+variable "vpc_id" {
+  type = string
+}
 
+variable "subnets" {
+  type = list(string)
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
